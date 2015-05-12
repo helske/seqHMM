2f641a0fff40098d5f1a777917c59a3ee29267b4
satuhelske <satu.helske@jyu.fi>
2015-05-12 13:26:20 +0300

    Modified convergence checks in trimHMM

--- R/trimHMM.R
+++ R/trimHMM.R
@@ -207,7 +207,7 @@ trimHMM<-function(model,maxit=0,return.loglik=FALSE,zerotol=1e-8,
     if(convergence.check==TRUE){
       fit<-fitHMM(model, optimx.control=list(kkt=TRUE),...)
       if(fit$optimx.result$convcode==0 && fit$optimx.result$kkt1==TRUE && fit$optimx.result$kkt2==TRUE){
-        print("Convergence check: (Local) maximum was found.")
+        print("Convergence check: (Local) optimum was found.")
       }else{
         print("Convergence check: Possible problem(s) with convergence.")
         if(fit$optimx.result$convcode!=0){
