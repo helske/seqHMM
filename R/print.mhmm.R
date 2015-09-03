c08cdff7bb43bf78656236c8fabd39f2d9b6a142
satuhelske <satu.helske@jyu.fi>
2015-09-03 15:16:55 +0300

    Deleted files

--- R/print.mhmm.R
+++
@@ -1,8 +0,0 @@
-#' @export
-#' @rdname print
-#' @method print hmm
-
-print.mhmm <- function(x, ...){
-  print(list(transition_matrix=x$transition_matrix, emission_matrix=x$emission_matrix, 
-             initial_probs=x$initial_probs, beta=x$beta))
-}
