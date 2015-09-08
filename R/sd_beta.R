2bb864b8442f299e8d2d9266d8512c2626c2e7c4
jouni.helske@jyu.fi <jouni.helske@jyu.fi>
2015-09-08 17:14:12 +0300

    formatted output

--- R/sd_beta.R
+++ R/sd_beta.R
@@ -1,3 +1,5 @@
 sd_beta <- function(model){
-  sqrt(diag(varcoef(model$beta, model$X, model$number_of_states)))
+  matrix(c(rep(0,model$number_of_covariates),
+    sqrt(diag(varcoef(model$beta, model$X, model$number_of_states)))),
+    nrow = model$number_of_covariates, ncol = model$number_of_clusters)
 }
