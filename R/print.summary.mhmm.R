2acf763ed115e320592e23190e9de611b0b84c5f
jouni.helske@jyu.fi <jouni.helske@jyu.fi>
2015-10-08 20:45:22 +0300

    removed extra empty spaces

--- R/print.summary.mhmm.R
+++ R/print.summary.mhmm.R
@@ -5,24 +5,22 @@
 print.summary.mhmm <- function(x, digits = 3, ...){
   if(exists("transition_matrix", x)){
     if(x$model$n_channels == 1){
-      cat("Initial probabilities :\n\n")
+      cat("Initial probabilities :\n")
       print.listof(x$initial_probs, digits = digits, ...)
-      cat("Transition probabilities :\n\n")
+      cat("Transition probabilities :\n")
       print.listof(x$transition_matrix, digits = digits, ...)
-      cat("Emission probabilities :\n\n")
+      cat("Emission probabilities :\n")
       print.listof(x$emission_matrix, digits = digits, ...)
-      cat("\n")
     }else{
-      cat("Initial probabilities :\n\n")
+      cat("Initial probabilities :\n")
       print.listof(x$initial_probs, digits = digits, ...)
-      cat("Transition matrix :\n\n")
+      cat("Transition probabilities :\n")
       print.listof(x$transition_matrix, digits = digits, ...)
-      cat("Emission matrix :\n\n")
+      cat("Emission probabilities :\n")
       for(i in 1:length(x$emission_matrix)){
         cat(names(x$emission_matrix)[i], ":\n\n")
         print.listof(x$emission_matrix[[i]],  digits = digits, ...)
       }
-      cat("\n")
     }
   }
   
@@ -38,13 +36,12 @@ print.summary.mhmm <- function(x, digits = 3, ...){
   cat("Covariate effects :\n")
   cat(x$model$cluster_names[1], "is the reference.\n\n")
   print.listof(coefs, print.gap = 2, digits = digits, ...)
-  cat("\n")
   
   cat("Log-likelihood:", x$logLik, "  BIC:", x$BIC, "\n\n")
   
   cat("Means of prior cluster probabilities :\n")
   print(colMeans(x$prior_cluster_probabilities), digits = digits, ...)
-  cat("\n\n")
+  cat("\n")
   
   tbl <- table(x$most_probable_cluster)
   cl <- matrix(c(as.character(tbl), 
@@ -54,7 +51,7 @@ print.summary.mhmm <- function(x, digits = 3, ...){
   rownames(cl) <- c("count", "proportion")
   cat("Most probable clusters :\n")
   print.default(cl, quote = FALSE, print.gap = 2, right = TRUE)
-  cat("\n\n")
+  cat("\n")
   
   cat("Classification table :\n")
   cat("Mean cluster probabilities (in columns) by the most probable cluster (rows)\n\n")
