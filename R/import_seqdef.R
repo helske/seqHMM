653e365f712ba57ae1cf7df5166ae79cd3452a11
Jouni Helske <jouni.helske@jyu.fi>
2015-11-02 09:34:04 +0200

    tidyed import documentation

--- R/import_seqdef.R
+++ R/import_seqdef.R
@@ -1,23 +1,16 @@
-#' Create a state sequence object
-#' 
-#' Imported function \code{\link[TraMineR]{seqdef}} from \code{TraMineR} for convinience.
+#' Imported functions from \code{TraMineR} for convinience.
+#'  
+#' For details, see corresponding help pages of \code{\link[TraMineR]{seqstatf}},
+#' \code{\link[TraMineR]{alphabet}} and \code{\link[TraMineR]{seqdef}}.
 #' 
 #' @name seqdef
 #' @export
 #' @rdname TraMineR_imports
 NULL
-#' State frequencies in the whole sequence data set
-#' 
-#' Imported function \code{\link[TraMineR]{seqstatf}} from \code{TraMineR} for convinience.
-#' 
 #' @name seqstatf
 #' @export
 #' @rdname TraMineR_imports
 NULL
-#' Get or set the alphabet of a sequence object
-#' 
-#' Imported function \code{\link[TraMineR]{alphabet}} from \code{TraMineR} for convinience.
-#' 
 #' @name alphabet
 #' @export
 #' @rdname TraMineR_imports
