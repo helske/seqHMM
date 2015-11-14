42a09a694da124736395ccb504b81674898285d5
jouni.helske@jyu.fi <jouni.helske@jyu.fi>
2015-11-14 14:59:11 +0200

    don't drop dimensions of single state model

--- R/mc_to_sc.R
+++ R/mc_to_sc.R
@@ -1,54 +1,54 @@
 #' Transform Multichannel Hidden Markov Model to Single Channel Representation
-#' 
+#'
 #' Transforms data and parameters of multichannel model to single channel model.
 #' Observed states (symbols) are combined and parameters multiplied accross channels.
-#' 
+#'
 #' @export
 #' @param model Object of class \code{hmm} or \code{mhmm}.
-#' @param combine_missing Controls whether combined states of observations are 
-#'   coded missing (NA) if some of the channels include missing information. 
+#' @param combine_missing Controls whether combined states of observations are
+#'   coded missing (NA) if some of the channels include missing information.
 #'   Defaults to \code{TRUE}.
 #' @param all_combinations Controls whether all possible combinations of
 #'   observed states are included in the single channel representation or only
 #'   combinations that are found in the data. Defaults to \code{FALSE}, i.e.
 #'   only actual observations are included.
-#'   
-#' @examples 
+#'
+#' @examples
 #' # Loading a hidden Markov model of the biofam data (hmm object)
 #' data(hmm_biofam)
-#' 
+#'
 #' sc <- mc_to_sc(hmm_biofam)
-#' 
-#' @seealso \code{\link{build_hmm}} and \code{\link{fit_hmm}} for building and 
-#'   fitting Hidden Markov models; and \code{\link{hmm_biofam}} for information on 
+#'
+#' @seealso \code{\link{build_hmm}} and \code{\link{fit_hmm}} for building and
+#'   fitting Hidden Markov models; and \code{\link{hmm_biofam}} for information on
 #'   the model used in the example;.
 
 mc_to_sc<-function(model, combine_missing=TRUE, all_combinations=FALSE){
-  
+
   if (model$n_channels == 1){
     return(model)
   }
-  
+
   if (inherits(model, "hmm")) {
-    
+
     B <- matrix(0,model$n_states,prod(model$n_symbols))
-    
+
     colnames(B) <- apply(
-      expand.grid(lapply(model$emission_probs,colnames)),                
+      expand.grid(lapply(model$emission_probs,colnames)),
       1,paste0,collapse="/")
     rownames(B) <- rownames(model$emission_probs[[1]])
     for (i in 1:model$n_states) {
-      B[i,] <- apply(expand.grid(lapply(model$emission_probs,function(x) x[i,])),1,prod)   
+      B[i,] <- apply(expand.grid(lapply(model$emission_probs,function(x) x[i,])),1,prod)
     }
-    B <- B[, order(colnames(B))]
-    
+    B <- B[, order(colnames(B)), drop = FALSE]
+
     modelx <- model
     modelx$emission_probs <- B
     modelx$n_symbols <- ncol(B)
     modelx$n_channels <- as.integer(1)
     modelx$symbol_names <- snames <- colnames(B)
     modelx$channel_names <- "Observations"
-    
+
     modelx$observations<-model$observations[[1]]
     for (i in 2:model$n_channels) {
       modelx$observations<-as.data.frame(mapply(paste, modelx$observations,
@@ -56,17 +56,17 @@ mc_to_sc<-function(model, combine_missing=TRUE, all_combinations=FALSE){
                                                 USE.NAMES=FALSE,SIMPLIFY=FALSE,
                                                 MoreArgs=list(sep="/")))
     }
-    names(modelx$observations) <- names(model$observations[[1]])   
+    names(modelx$observations) <- names(model$observations[[1]])
     if (combine_missing == TRUE) {
       modelx$observations[Reduce("|",
                                  lapply(
-                                   model$observations, 
-                                   function(x) 
+                                   model$observations,
+                                   function(x)
                                      x==attr(model$observations[[1]], "nr") |
                                      x==attr(model$observations[[1]], "void") |
                                      is.na(x)))]<-NA
     }
-    
+
     if (modelx$n_symbols <= 200) {
       cpal <- seqHMM::colorpalette[[modelx$n_symbols]]
     } else {
@@ -80,57 +80,57 @@ mc_to_sc<-function(model, combine_missing=TRUE, all_combinations=FALSE){
       }
       cpal <- cp[1:modelx$n_symbols]
     }
-    
-    
+
+
     if (all_combinations == TRUE) {
       modelx$observations <- suppressWarnings(suppressMessages(seqdef(modelx$observations, alphabet=modelx$symbol_names)))
     } else {
       modelx$observations <- suppressWarnings(suppressMessages((seqdef(modelx$observations))))
-      modelx$emission_probs <- modelx$emission_probs[, colnames(modelx$emission_probs) %in% 
+      modelx$emission_probs <- modelx$emission_probs[, colnames(modelx$emission_probs) %in%
                                                        alphabet(modelx$observations) == TRUE]
       modelx$symbol_names <- colnames(modelx$emission_probs)
       modelx$n_symbols <- ncol(modelx$emission_probs)
     }
-    
+
     # mhmm
   } else {
-    
+
     modelx <- model
-    
+
     B <- vector("list", model$n_clusters)
     for (m in 1:model$n_clusters) {
       B[[m]] <- matrix(0, model$n_states[m], prod(model$n_symbols))
-      
+
       colnames(B[[m]])<-apply(
-        expand.grid(lapply(model$emission_probs[[m]],colnames)),                
+        expand.grid(lapply(model$emission_probs[[m]],colnames)),
         1,paste0,collapse="/")
       rownames(B[[m]])<-rownames(model$emission_probs[[m]][[1]])
       for(i in 1:model$n_states[[m]]){
-        B[[m]][i,]<-apply(expand.grid(lapply(model$emission_probs[[m]],function(x) x[i,])),1,prod)   
+        B[[m]][i,]<-apply(expand.grid(lapply(model$emission_probs[[m]],function(x) x[i,])),1,prod)
       }
-      B[[m]] <- B[[m]][, order(colnames(B[[m]]))]
-      
+      B[[m]] <- B[[m]][, order(colnames(B[[m]])), drop = FALSE]
+
       modelx$emission_probs[[m]] <- B[[m]]
     }
-    
+
     modelx$n_symbols <- ncol(B[[1]])
     modelx$n_channels <- as.integer(1)
     modelx$symbol_names <- snames <- colnames(B[[1]])
-    
+
     modelx$channel_names <- "Observations"
-    
+
     modelx$observations <- model$observations[[1]]
     for(i in 2:model$n_channels)
       modelx$observations <- as.data.frame(mapply(paste, modelx$observations,
                                                   model$observations[[i]],
                                                   USE.NAMES=FALSE,SIMPLIFY=FALSE,
                                                   MoreArgs=list(sep="/")))
-    names(modelx$observations) <- names(model$observations[[1]])   
+    names(modelx$observations) <- names(model$observations[[1]])
     if(combine_missing==TRUE){
       modelx$observations[Reduce("|",
                                  lapply(
-                                   model$observations, 
-                                   function(x) 
+                                   model$observations,
+                                   function(x)
                                      x==attr(model$observations[[1]], "nr") |
                                      x==attr(model$observations[[1]], "void") |
                                      is.na(x)))]<-NA
@@ -148,21 +148,21 @@ mc_to_sc<-function(model, combine_missing=TRUE, all_combinations=FALSE){
       }
       cpal <- cp[1:modelx$n_symbols]
     }
-    
-    
+
+
     if (all_combinations == TRUE) {
       modelx$observations <- suppressWarnings(suppressMessages(seqdef(modelx$observations, alphabet=modelx$symbol_names)))
     } else {
       modelx$observations <- suppressWarnings(suppressMessages((seqdef(modelx$observations))))
       for (m in 1:model$n_clusters){
-        modelx$emission_probs[[m]] <- modelx$emission_probs[[m]][, colnames(modelx$emission_probs[[m]]) %in% 
+        modelx$emission_probs[[m]] <- modelx$emission_probs[[m]][, colnames(modelx$emission_probs[[m]]) %in%
                                                          alphabet(modelx$observations) == TRUE]
       }
       modelx$symbol_names <- colnames(modelx$emission_probs[[1]])
       modelx$n_symbols <- ncol(modelx$emission_probs[[1]])
     }
   }
-  
+
   attr(modelx$observations, "xtstep") <- attr(model$observations[[1]], "xtstep")
   attr(modelx$observations, "missing.color") <- attr(model$observations[[1]], "missing.color")
   attr(modelx$observations, "nr") <- attr(model$observations[[1]], "nr")
