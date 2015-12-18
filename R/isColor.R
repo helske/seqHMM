eeb95780f4a18c00bfcff046c626619f76da850e
Jouni Helske <jouni.helske@jyu.fi>
2015-12-18 11:27:26 +0200

    add refs and update description

--- R/isColor.R
+++ R/isColor.R
@@ -1,6 +1,7 @@
+# Copied from a post by Josh O'Brien on Stack Overflow:
+# http://stackoverflow.com/questions/13289009/check-if-character-string-is-a-valid-color-representation
 isColor <- function(x) {
   sapply(x, function(X) {
-    tryCatch(is.matrix(col2rgb(X)), 
-             error = function(e) FALSE)
+    tryCatch(is.matrix(col2rgb(X)), error = function(e) FALSE)
   })
 }
