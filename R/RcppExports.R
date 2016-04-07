4f3705580364890cfeb9f2690f4146310a04a747
helske <jouni.helske@jyu.fi>
2016-04-07 11:10:19 +0300

    updated reltol

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -17,6 +17,18 @@ forwardbackwardx <- function(transition, emissionArray, init, obsArray, coef, X,
     .Call('seqHMM_forwardbackwardx', PACKAGE = 'seqHMM', transition, emissionArray, init, obsArray, coef, X, numberOfStates, forwardonly, threads)
 }
 
+logLikHMM <- function(transition, emissionArray, init, obsArray, threads) {
+    .Call('seqHMM_logLikHMM', PACKAGE = 'seqHMM', transition, emissionArray, init, obsArray, threads)
+}
+
+logLikMixHMM <- function(transition, emissionArray, init, obsArray, coef, X, numberOfStates, threads) {
+    .Call('seqHMM_logLikMixHMM', PACKAGE = 'seqHMM', transition, emissionArray, init, obsArray, coef, X, numberOfStates, threads)
+}
+
+logSumExp <- function(x) {
+    .Call('seqHMM_logSumExp', PACKAGE = 'seqHMM', x)
+}
+
 log_EM <- function(transitionMatrix, emissionArray, initialProbs, obsArray, nSymbols, itermax, tol, trace, threads) {
     .Call('seqHMM_log_EM', PACKAGE = 'seqHMM', transitionMatrix, emissionArray, initialProbs, obsArray, nSymbols, itermax, tol, trace, threads)
 }
@@ -47,18 +59,6 @@ log_objective <- function(transition, emissionArray, init, obsArray, ANZ, emissN
 
 log_objectivex <- function(transition, emissionArray, init, obsArray, ANZ, emissNZ, INZ, nSymbols, coef, X, numberOfStates, threads) {
     .Call('seqHMM_log_objectivex', PACKAGE = 'seqHMM', transition, emissionArray, init, obsArray, ANZ, emissNZ, INZ, nSymbols, coef, X, numberOfStates, threads)
-}
-
-logLikHMM <- function(transition, emissionArray, init, obsArray, threads) {
-    .Call('seqHMM_logLikHMM', PACKAGE = 'seqHMM', transition, emissionArray, init, obsArray, threads)
-}
-
-logLikMixHMM <- function(transition, emissionArray, init, obsArray, coef, X, numberOfStates, threads) {
-    .Call('seqHMM_logLikMixHMM', PACKAGE = 'seqHMM', transition, emissionArray, init, obsArray, coef, X, numberOfStates, threads)
-}
-
-logSumExp <- function(x) {
-    .Call('seqHMM_logSumExp', PACKAGE = 'seqHMM', x)
 }
 
 objective <- function(transition, emissionArray, init, obsArray, ANZ, emissNZ, INZ, nSymbols, threads) {
