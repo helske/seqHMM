be3a189d87e3409bbcba2d50c16bb34a82affaa4
Jouni Helske <jouni.helske@jyu.fi>
2016-06-03 08:01:16 +0300

    testing differences

--- src/forwardbackwardx.cpp
+++ src/forwardbackwardx.cpp
@@ -6,26 +6,31 @@
 List forwardbackwardx(const arma::mat& transition, NumericVector emissionArray,
   const arma::vec& init, IntegerVector obsArray, const arma::mat& coef, const arma::mat& X,
   const arma::ivec& numberOfStates, bool forwardonly, int threads) {
-  
+
   IntegerVector eDims = emissionArray.attr("dim"); //m,p,r
   IntegerVector oDims = obsArray.attr("dim"); //r,n,k
-  
+
   arma::cube emission(emissionArray.begin(), eDims[0], eDims[1], eDims[2], false, true);
   arma::icube obs(obsArray.begin(), oDims[0], oDims[1], oDims[2], false, true);
-  
+
   arma::mat weights = exp(X * coef).t();
   weights.each_row() /= arma::sum(weights, 0);
-  
+
   arma::mat initk(emission.n_rows, obs.n_slices);
   for (unsigned int k = 0; k < obs.n_slices; k++) {
     initk.col(k) = init % reparma(weights.col(k), numberOfStates);
   }
   arma::cube alpha(emission.n_rows, obs.n_cols, obs.n_slices); //m,n,k
   arma::mat scales(obs.n_cols, obs.n_slices); //m,n,k
-  
-  arma::sp_mat sp_trans(transition);
-  internalForwardx(sp_trans.t(), emission, initk, obs, alpha, scales, threads);
 
+  arma::sp_mat sp_trans(transition);
+  for (unsigned int k = 0; k < obs.n_slices; k++) {
+    arma::mat atmp(emission.n_rows, obs.n_cols);
+    arma::vec stmp(obs.n_cols);
+  uvForward(sp_trans.t(), emission, initk.col(k), obs.slice(k), atmp, stmp);
+alpha.slice(k) = atmp;
+scales.col(k) = stmp;
+  }
   if(!scales.is_finite()) {
     Rcpp::stop("Scaling factors contain non-finite values. Check the model or try using the log-space version of the algorithm.");
   }
@@ -33,19 +38,25 @@ List forwardbackwardx(const arma::mat& transition, NumericVector emissionArray,
   if (min_sf < 1e-150) {
     Rcpp::warning("Smallest scaling factor was %e, results can be numerically unstable.", min_sf);
   }
-  
+
   if (forwardonly) {
     return List::create(Named("forward_probs") = wrap(alpha),
       Named("scaling_factors") = wrap(scales));
   } else {
     arma::cube beta(emission.n_rows, obs.n_cols, obs.n_slices); //m,n,k
-    internalBackwardx(sp_trans, emission, obs, beta, scales, threads);
+    for (unsigned int k = 0; k < obs.n_slices; k++) {
+      arma::mat btmp(emission.n_rows, obs.n_cols);
+      arma::vec stmp(obs.n_cols);
+      stmp = scales.col(k);
+      uvBackward(sp_trans, emission, obs.slice(k), btmp, stmp);
+      beta.slice(k) = btmp;
+    }
     if(!beta.is_finite()) {
       Rcpp::stop("Backward probabilities contain non-finite values. Check the model or try using the log-space version of the algorithm.");
     }
     return List::create(Named("forward_probs") = wrap(alpha), Named("backward_probs") = wrap(beta),
       Named("scaling_factors") = wrap(scales));
   }
-  
+
   return wrap(alpha);
 }
