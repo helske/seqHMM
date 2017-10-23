4c0399d7458a0ab388185ee77dab1b681f278fb4
Jouni Helske <jouni.helske@iki.fi>
2017-10-23 15:54:39 +0200

    openmp back

--- src/seqHMM.h
+++ src/seqHMM.h
@@ -1,10 +1,11 @@
 #ifndef SEQHMM_H
 #define SEQHMM_H
 
-// // [[Rcpp::plugins(openmp)]]
-// #ifdef _OPENMP
-// #include <omp.h>
-// #endif
+
+#ifdef _OPENMP
+// [[Rcpp::plugins(openmp)]]
+#include <omp.h>
+#endif
 
 #define ARMA_NO_DEBUG
 #define ARMA_DONT_PRINT_ERRORS
