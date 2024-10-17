b3318ba57c368ffdd9560fba333822c82b9fd805
Jouni Helske <jouni.helske@utu.fi>
2024-10-17 09:20:27 +0300

    remove unused variable

--- src/get_parameters.cpp
+++ src/get_parameters.cpp
@@ -40,7 +40,6 @@ arma::vec get_log_pi(const arma::mat& gamma, const arma::vec& X) {
 arma::cube get_A(const arma::cube& gamma, const arma::mat& X, 
                  const bool tv) {
   unsigned int S = gamma.n_slices;
-  unsigned int K = X.n_rows;
   unsigned int T = X.n_cols;
   arma::cube A(S, S, T);
   arma::mat Atmp(S, S);
@@ -65,7 +64,6 @@ arma::cube get_A(const arma::cube& gamma, const arma::mat& X,
 arma::cube get_log_A(const arma::cube& gamma, const arma::mat& X, 
                      const bool tv) {
   unsigned int S = gamma.n_slices;
-  unsigned int K = X.n_rows;
   unsigned int T = X.n_cols;
   arma::cube A(S, S, T);
   arma::mat Atmp(S, S);
@@ -91,7 +89,6 @@ arma::cube get_B(const arma::cube& gamma, const arma::mat& X,
                  const bool tv, const bool add_missing) {
   unsigned int S = gamma.n_slices;
   unsigned int M = gamma.n_rows;
-  unsigned int K = X.n_rows;
   unsigned int T = X.n_cols;
   arma::cube B(S, M + add_missing, T);
   arma::mat Btmp(M + add_missing, S);
@@ -123,7 +120,7 @@ arma::field<arma::cube> get_B(
   unsigned int C = M.n_elem;
   arma::field<arma::cube> B(C); // C field of cubes, each S x M_c x T
   for (unsigned int c = 0; c < C; c++) {
-    B(c) = get_B(gamma(c), X, add_missing, tv);
+    B(c) = get_B(gamma(c), X, tv, add_missing);
   }
   return B;
 }
@@ -131,10 +128,9 @@ arma::field<arma::cube> get_B(
 // X is K x T (covariates, time points)
 // [[Rcpp::export]]
 arma::cube get_log_B(const arma::cube& gamma, const arma::mat& X, 
-                     const bool add_missing, const bool tv) {
+                     const bool tv, const bool add_missing) {
   unsigned int S = gamma.n_slices;
   unsigned int M = gamma.n_rows;
-  unsigned int K = X.n_rows;
   unsigned int T = X.n_cols;
   arma::cube B(S, M + add_missing, T);
   arma::mat Btmp(M + add_missing, S);
@@ -162,11 +158,11 @@ arma::cube get_log_B(const arma::cube& gamma, const arma::mat& X,
 // X is K x T (covariates, time point)
 arma::field<arma::cube> get_log_B(
     const arma::field<arma::cube>& gamma, const arma::mat& X, 
-    const arma::uvec& M, const bool add_missing, const bool tv) {
+    const arma::uvec& M, const bool tv, const bool add_missing) {
   unsigned int C = M.n_elem;
   arma::field<arma::cube> log_B(C); // C field of cubes, each S x M_c x T
   for (unsigned int c = 0; c < C; c++) {
-    log_B(c) = get_log_B(gamma(c), X, add_missing, tv);
+    log_B(c) = get_log_B(gamma(c), X, tv, add_missing);
   }
   return log_B;
 }
@@ -197,12 +193,11 @@ arma::field<arma::cube> get_A_all(const arma::cube& gamma, const arma::cube& X,
 // X is K x T (covariates, time points)
 // [[Rcpp::export]]
 arma::field<arma::cube> get_B_all(
-    const arma::cube& gamma,  const arma::cube& X, 
-    const bool add_missing, const bool tv) {
+    const arma::cube& gamma,  const arma::cube& X, const bool tv) {
   unsigned int N = X.n_slices;
   arma::field<arma::cube> B(N);
   for (unsigned int i = 0; i < N; i++) {
-    B(i) = get_B(gamma, X.slice(i), add_missing, tv);
+    B(i) = get_B(gamma, X.slice(i), tv);
   }
   return B;
 }
@@ -212,17 +207,17 @@ arma::field<arma::cube> get_B_all(
 // probs is a vector of length P
 // output is S x N x P cube
 // [[Rcpp::export]]
-arma::mat get_pi_qs(const arma::cube& gamma, const arma::mat& X, 
+arma::mat get_pi_qs(const arma::field<arma::mat>& gamma, const arma::mat& X, 
                     const arma::vec& probs) {
-  unsigned int S = gamma.n_rows;
-  unsigned int L = gamma.n_slices;
+  unsigned int S = gamma(0).n_rows;
+  unsigned int L = gamma.n_elem;
   unsigned int N = X.n_cols;
   unsigned int P = probs.n_elem;
   arma::mat pi(S, L);
   arma::mat qs(S * N, P);
   for (unsigned int i = 0; i < N; i++) {
     for (unsigned int l = 0; l < L; l++) {
-      pi.col(l) = get_pi(gamma.slice(l), X.col(i));
+      pi.col(l) = get_pi(gamma(l), X.col(i));
     }
     qs.rows(i * S, (i + 1) * S - 1) = arma::quantile(pi, probs, 1);
   }
@@ -272,7 +267,7 @@ arma::mat get_B_qs(const arma::field<arma::cube>& gamma,
   arma::mat qs(SMT * N, P);
   for (unsigned int i = 0; i < N; i++) {
     for (unsigned int l = 0; l < L; l++) {
-      B.col(l) = arma::vectorise(get_B(gamma(l), X.slice(i), tv, false));
+      B.col(l) = arma::vectorise(get_B(gamma(l), X.slice(i), tv));
     }
     qs.rows(i * SMT, (i + 1) * SMT - 1) = arma::quantile(B, probs, 1);
   }
@@ -282,17 +277,17 @@ arma::mat get_B_qs(const arma::field<arma::cube>& gamma,
 // X a K x N matrix
 // output is D x N x B cube
 // [[Rcpp::export]]
-arma::mat get_omega_qs(const arma::cube& gamma, const arma::mat& X,
+arma::mat get_omega_qs(const arma::field<arma::mat>& gamma, const arma::mat& X,
                        const arma::vec& probs) {
-  unsigned int D = gamma.n_rows;
-  unsigned int L = gamma.n_slices;
+  unsigned int D = gamma(0).n_rows;
+  unsigned int L = gamma.n_elem;
   unsigned int N = X.n_cols;
   unsigned int P = probs.n_elem;
   arma::mat omega(D, L);
   arma::mat qs(D * N, P);
   for (unsigned int i = 0; i < N; i++) {
     for (unsigned int l = 0; l < L; l++) {
-      omega.col(l) = get_omega(gamma.slice(l), X.col(i));
+      omega.col(l) = get_omega(gamma(l), X.col(i));
     }
     qs.rows(i * D, (i + 1) * D - 1) = arma::quantile(omega, probs, 1);
   }
@@ -304,26 +299,23 @@ arma::mat get_omega_qs(const arma::cube& gamma, const arma::mat& X,
 // X a K x N matrix
 // probs is a vector of length P
 // [[Rcpp::export]]
-arma::mat get_pi_ame(const arma::cube& gamma, 
+arma::mat get_pi_ame(const arma::field<arma::mat>& gamma, 
                      const arma::mat& X1, const arma::mat& X2, 
                      const arma::vec& probs) {
-  unsigned int S = gamma.n_rows;
-  unsigned int L = gamma.n_slices;
+  unsigned int S = gamma(0).n_rows;
+  unsigned int L = gamma.n_elem;
   unsigned int N = X1.n_cols;
   unsigned int P = probs.n_elem;
   double invN = 1.0 / N;
   arma::mat pi(S, L, arma::fill::zeros);
-  arma::mat qs(S, P + 1);
   for (unsigned int l = 0; l < L; l++) {
     for (unsigned int i = 0; i < N; i++) {
       pi.col(l) += invN * (
-        get_pi(gamma.slice(l), X1.col(i)) - get_pi(gamma.slice(l), X2.col(i))
+        get_pi(gamma(l), X1.col(i)) - get_pi(gamma(l), X2.col(i))
       );
     }
   }
-  qs.col(0) = arma::mean(pi, 1);
-  qs.cols(1, P) = arma::quantile(pi, probs, 1);
-  return qs;
+  return arma::quantile(pi, probs, 1);;
 }
 // gamma is L field of S x K x S cubes
 // X is K x T x N cube (covariates, time points, sequences)
@@ -341,7 +333,6 @@ arma::mat get_A_ame(const arma::field<arma::cube>& gamma,
   double invN = 1.0 / N;
   unsigned int SST = S * S * T;
   arma::mat A(SST, L, arma::fill::zeros);
-  arma::mat qs(SST, P + 1);
   for (unsigned int l = 0; l < L; l++) {
     for (unsigned int i = 0; i < N; i++) {
       A.col(l) += invN * (
@@ -350,9 +341,7 @@ arma::mat get_A_ame(const arma::field<arma::cube>& gamma,
       );
     }
   }
-  qs.col(0) = arma::mean(A, 1);
-  qs.cols(1, P) = arma::quantile(A, probs, 1);
-  return qs;
+  return arma::quantile(A, probs, 1);
 }
 
 // gamma is L field of M x K x S cubes
@@ -370,7 +359,6 @@ arma::mat get_B_ame(const arma::field<arma::cube>& gamma,
   unsigned int SMT = S * M * T;
   double invN = 1.0 / N;
   arma::mat B(SMT, L, arma::fill::zeros);
-  arma::mat qs(SMT, P);
   for (unsigned int l = 0; l < L; l++) {
     for (unsigned int i = 0; i < N; i++) {
       B.col(l) += invN * (
@@ -379,31 +367,26 @@ arma::mat get_B_ame(const arma::field<arma::cube>& gamma,
       );
     }
   }
-  qs.col(0) = arma::mean(B, 1);
-  qs.cols(1, P) = arma::quantile(B, probs, 1);
-  return qs;
+  return arma::quantile(B, probs, 1);
 }
 // gamma is D x K x L (cluster, covariates, sample)
 // X a K x N matrix
 // [[Rcpp::export]]
-arma::mat get_omega_ame(const arma::cube& gamma, 
+arma::mat get_omega_ame(const arma::field<arma::mat>& gamma, 
                      const arma::mat& X1, const arma::mat& X2, 
                      const arma::vec& probs) {
-  unsigned int D = gamma.n_rows;
-  unsigned int L = gamma.n_slices;
+  unsigned int D = gamma(0).n_rows;
+  unsigned int L = gamma.n_elem;
   unsigned int N = X1.n_cols;
   unsigned int P = probs.n_elem;
   double invN = 1.0 / N;
   arma::mat omega(D, L, arma::fill::zeros);
-  arma::mat qs(D, P + 1);
   for (unsigned int l = 0; l < L; l++) {
     for (unsigned int i = 0; i < N; i++) {
       omega.col(l) += invN * (
-        get_omega(gamma.slice(l), X1.col(i)) - get_omega(gamma.slice(l), X2.col(i))
+        get_omega(gamma(l), X1.col(i)) - get_omega(gamma(l), X2.col(i))
       );
     }
   }
-  qs.col(0) = arma::mean(omega, 1);
-  qs.cols(1, P) = arma::quantile(omega, probs, 1);
-  return qs;
+  return arma::quantile(omega, probs, 1);
 }
